0ff4c42a503f1268c034470859d375dbf96d057c
JadeXTGitHub <78270947+JadeXTGitHub@users.noreply.github.com>
2021-04-19 19:32:25 -0500

    Add files via upload

---
+++ alert.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>Lab Session 14 - Jennifer Thomas</title>
+<meta charset="utf-8">
+</head>
+<body>
+<h1>Using JavaScript</h1>
+<script>
+ alert("Welcome to my web page!");
+</script>
+<h2>When does this display?</h2>
+</body>
+</html>
